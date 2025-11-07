d1bf08840738b11127a21bdcddc8c1c6cc3a639d
[dyad] <foxbox.shoppingdz@gmail.com>
2025-11-07 09:08:33 +0100

    [dyad] wrote 1 file(s)

--- src/components/ChocolateDivider.tsx
+++ src/components/ChocolateDivider.tsx
@@ -6,7 +6,6 @@ const ChocolateDivider = () => {
   return (
     <svg
       data-name="Layer 1"
-      xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 1200 120"
       preserveAspectRatio="none"
       className="block w-full h-24" {/* Set fixed height */}
